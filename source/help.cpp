92fa4344882f6295d72ecf945183b8903d059caa
Ilya Kryukov <ilyakrukov@gmail.com>
2023-02-12 18:27:23 +0400

    initial source drop

---
+++ source/help.cpp
@@ -0,0 +1,6 @@
+// Generate cpp implementation
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
